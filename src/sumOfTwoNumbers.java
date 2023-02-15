d8173dc8bb4ede60ebf949b020e2f646cbd7c773
sweetCaro <jp2020213614@qmul.ac.uk>
2023-02-15 20:30:09 +0800

    两数之和

--- src/sumOfTwoNumbers.java
+++ src/sumOfTwoNumbers.java
@@ -1,19 +1,27 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class sumOfTwoNumbers {
 
     public static int[] twoSum(int[] nums, int target){
-        Map<Integer, Integer> hashtable = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
-            if (hashtable.containsKey(target - nums[i])) {
-                return new int[]{hashtable.get(target - nums[i]), i};
+            for(int i = 0; i < nums.length-1; i++){
+                for(int j = i; j < nums.length; j++){
+                    if(nums[i]==target-nums[j]){
+                        return new int[]{i, j};
+                    }
+                }
             }
-            hashtable.put(nums[i], i);
+            return new int[0];
         }
-        return new int[0];
-    }
+        /**哈希解法
+         * Map<Integer, Integer> hashtable = new HashMap<>();
+         *         for (int i = 0; i < nums.length; ++i) {
+         *             if (hashtable.containsKey(target - nums[i])) {
+         *                 return new int[]{hashtable.get(target - nums[i]), i};
+         *             }
+         *             hashtable.put(nums[i], i);
+         *         }
+         *         return new int[0];
+        */
 
     public static void main(String[] args){
         int[] nums = {2,7,11,15};
