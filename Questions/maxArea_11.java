685ec6cec4db8c8f581cc9ca06ec3da92a362efa
sweetCaro <jp2020213614@qmul.ac.uk>
2023-03-01 19:36:00 +0800

    盛最多水的容器

--- Questions/maxArea_11.java
+++ Questions/maxArea_11.java
@@ -3,23 +3,39 @@ import java.util.Comparator;
 
 public class maxArea_11 {
     public static int maxArea(int[] height){
-        int result=0;
 
-        ArrayList<Integer> maxArea = new ArrayList<>();
-        for(int i=0; i<height.length; i++){
-            for(int j=i;j<height.length;j++){
-                if(height[i]>=height[j]){
-                    maxArea.add(height[j]*(j-i));
-                }
-                if(height[i]<height[j]){
-                    maxArea.add(height[i]*(j-i));
-                }
+//        ArrayList<Integer> maxArea = new ArrayList<>();
+//        for(int i=0; i<height.length; i++){
+//            for(int j=i;j<height.length;j++){
+//                if(height[i]>=height[j]){
+//                    maxArea.add(height[j]*(j-i));
+//                }
+//                if(height[i]<height[j]){
+//                    maxArea.add(height[i]*(j-i));
+//                }
+//            }
+//
+//        }
+//        maxArea.sort(Comparator.reverseOrder());
+//        result = maxArea.get(0);
+//        //用arraylist来做，超出内存限制了
+        int result=0;
+        int left =0;
+        int right = height.length-1;
+        int area=0;
+        while(left<right){
+            area=Math.min(height[left],height[right])*(right-left);
+            result=Math.max(result,area);
+
+            if(height[left]>=height[right]){
+                left++;
+            }
+            else{
+                right--;
             }
 
         }
-        maxArea.sort(Comparator.reverseOrder());
-        result = maxArea.get(0);
-        //用arraylist来做，超出内存限制了
+
         return result;
     }
     public static void main(String[] args) {
